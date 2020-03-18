50874f2bd083ec0d77975217e07ef41bbe2f890f
xaraxx <sara.galvan.o91@gmail.com>
2020-03-18 00:31:54 -0500

    debuging and including file from platzi

--- main2.py
+++ main2.py
@@ -1,5 +1,5 @@
-import sys
 
+import sys
 
 clients = [
     {
@@ -15,28 +15,29 @@ clients = [
         'position': 'Data Engineer',
     },
 ]
-
-
-def create_client(client):
-    global clients
-
-    if client not in clients:
-        clients.append(client)
-    else:
-        print('Client already in client\'s list')
-
+    
 
 def list_clients():
     print('uid |  name  | company  | email  | position ')
     print('*' * 50)
-
+    
     for idx, client in enumerate(clients):
         print('{uid} | {name} | {company} | {email} | {position}'.format(
             uid=idx, 
-            name=client['name'], 
-            company=client['company'], 
-            email=client['email'], 
+            name=client['name'],
+            company=client['company'],
+            email=client['email'],
             position=client['position']))
+
+
+def create_clients(client):
+    global clients
+
+    if client not in clients:
+        clients.append(client)
+ 
+    else:
+        print('The client already exist in the data base.')
 
 
 def update_client(client_id, updated_client):
@@ -45,17 +46,20 @@ def update_client(client_id, updated_client):
     if len(clients) - 1 >= client_id:
         clients[client_id] = updated_client
     else:
-        print('Client not in client\'s list')
-
+        client_name = client['name']
+        _print_not_name_in_client_list(client_name)
 
 def delete_client(client_id):
     global clients
 
     for idx, client in enumerate(clients):
-        if idx == client_id:
-            del clients[idx] 
-            break
 
+        if idx == client_id:
+            del clients['idx']
+            break
+        else:
+            client_name = client['name']
+            _print_not_name_in_client_list(client_name)
 
 def search_client(client_name):
     for client in clients:
@@ -64,50 +68,78 @@ def search_client(client_name):
         else:
             return True
 
+def _add_comma():
+    global clients
+    clients += ','
 
-def _get_client_field(field_name, message='What is the client {}?'):
+
+def _print_welcome():
+    print('*' * 48)
+    print('* W E L C O M E  TO  P L A T Z I - V E N T A S *')
+    print('*' * 48)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[U]pdate client')
+    print('[D]elete client')
+    print('[L]ist client')
+    print('[S]earch client')
+
+    print('*' * 48)
+
+
+def _print_invalid():
+    print('The command used is invalid.')
+
+def _print_not_name_in_client_list(client_name):
+    print('The Client: {} is not in clients list'.format(client_name))
+
+
+def _print_name_in_client_list():
+    print('The client is in clients list')
+
+def _get_client_field(field_name):
     field = None
 
     while not field:
-        field = input(message.format(field_name))
+        field = input('What\'s the client {}?'.format(field_name))
 
     return field
-
 
 def _get_client_from_user():
     client = {
         'name': _get_client_field('name'),
         'company': _get_client_field('company'),
         'email': _get_client_field('email'),
-        'position': _get_client_field('position'),
+        'position': _get_client_field('position')
     }
 
     return client
 
 
-def _print_welcome():
-    print('WELCOME TO PLATZI VENTAS')
-    print('*' * 50)
-    print('What would you like to do today?:')
-    print('[C]reate client')
-    print('[L]ist clients')
-    print('[U]pdate client')
-    print('[D]elete client')
-    print('[S]earch client')
+# def _get_client_name():
+#     client_name = None
+
+#     while not client_name:
+#         client_name = input('What\'s the client name? ')
+
+#         if client_name == 'exit':
+#             print('this conditional')
+#             break
+
+#     if not client_name:
+#         sys.exit()
+
+#     return client_name
 
 
 if __name__ == '__main__':
     _print_welcome()
 
-    command = input()
-    command = command.upper()
-
+    command = input().upper()
+    
     if command == 'C':
         client = _get_client_from_user()
-
-        create_client(client)
-        list_clients()
-    elif command == 'L':
+        create_clients(client)
         list_clients()
     elif command == 'U':
         client_id = int(_get_client_field('id'))
@@ -120,14 +152,15 @@ if __name__ == '__main__':
 
         delete_client(client_id)
         list_clients()
+    elif command == 'L':
+        list_clients()
     elif command == 'S':
         client_name = _get_client_field('name')
         found = search_client(client_name)
-        
-        if found:
-            print('The client is in the client\'s list')
-        else:
-            print('The client: {} is not in our client\'s list'.format(client_name))
-    else:
-        print('Invalid command')
 
+        if found:
+            _print_name_in_client_list()
+        else:
+            _print_not_name_in_client_list(client_name)
+    else:
+        _print_invalid()
